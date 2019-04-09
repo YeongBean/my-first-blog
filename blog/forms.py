92c1308fa6d5faed68550c8b79b9d7a31f5ed77e
BEAN <nogadahalf12@naver.com>
2019-04-10 01:35:20 +0900

    final

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,9 @@
-from django.shortcuts import render
-from django.utils import timezone
+from django import forms
+
 from .models import Post
-from django.shortcuts import render, get_object_or_404
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostForm(forms.ModelForm):
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    class Meta:
+        model = Post
+        fields = ('title', 'text',)
